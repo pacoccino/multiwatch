92853e18d77c12b135ec1cb9c6c878906a191c1e
Paco <pakokrew@gmail.com>
2017-06-26 14:42:57 +0200

    remote proxy

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,6 +1,6 @@
 import superagent from 'superagent';
 
-const CORS_PROXY = 'http://localhost:3001/cors?url=';
+const CORS_PROXY = 'https://cors-proxy-ngfar.herokuapp.com/cors?url=';
 
 export const request = url => superagent(`${CORS_PROXY}${url}`).then(res => res.body);
 export const cwRequest = url => request(url).then(res => res.result);
